852156c182c3a52e097727faee024884ab810dc8
Jamie Winsor <jamie@vialstudios.com>
2012-03-29 23:55:25 -0700

    alias RubyWMI to WMI

--- lib/ruby-wmi.rb
+++ lib/ruby-wmi.rb
@@ -5,7 +5,7 @@ module RubyWMI
   autoload :privilege, 'ruby-wmi/privilege'
 end
 
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 require 'ruby-wmi/base'
+
+# Alias for {RubyWMI}
+WMI = RubyWMI
